68c00405ef0687b0dfabcceb84841fc39ffc82c0
hilee93 <eldh2002@gmail.com>
2025-10-22 09:30:40 +0900

    Revert "[이형일] sprint2"

--- src/main/java/com/sprint/mission/discodeit/entity/UserRole.java
+++
@@ -1,7 +0,0 @@
-package com.sprint.mission.discodeit.entity;
-
-public enum UserRole {
-    OWNER,
-    MEMBER,
-    ADMIN
-}
--- src/main/java/com/sprint/mission/discodeit/entity/UserState.java
+++
@@ -1,6 +0,0 @@
-package com.sprint.mission.discodeit.entity;
-
-public enum UserState {
-    OFFLINE,
-    ONLINE
-}
